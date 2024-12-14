cdbc19de8e1be99e59d9ebfb4a5273e932fae6ce
Egba Frank <Egbafrank72@gmail.com>
2024-12-14 12:43:07 +0000

    feat (#1): Set up backend server and directory structure

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: false,
+    node: true,  // Enable Node.js globals like process, module
+    es6: true,   // Enable ES6 syntax
+    jest: true,  // Enable Jest environment if you're using Jest for tests
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',  // Includes Jest linting rules if you're using Jest for testing
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,    // Enable parsing of modern JavaScript syntax
+    sourceType: 'module', // Enable ES modules (import/export)
+  },
+  plugins: ['jest'],  // Jest plugin for linting tests
+  rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',  // Exclude certain files like Babel config
+    },
+  ],
+};
