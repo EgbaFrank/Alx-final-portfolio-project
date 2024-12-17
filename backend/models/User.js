1acaa86ca5480d354e03364692de2b6110350169
Egba Frank <Egbafrank72@gmail.com>
2024-12-17 03:01:42 +0000

    fix: change arrow function to regular function to preserve this context

--- backend/models/User.js
+++ backend/models/User.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
   },
 }, { timestamps: true });
 
-userSchema.pre('save', async (next) => {
+userSchema.pre('save', async function (next) {
   if (this.isModified('password') || this.isNew) {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
