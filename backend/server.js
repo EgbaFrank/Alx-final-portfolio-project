c12cd80c5fc64b65f02c30dba6719458f75c1f9c
Egba Frank <Egbafrank72@gmail.com>
2024-12-17 02:58:43 +0000

    chore: improve output formatting and database connection call

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,33 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './utils/db.js';
 
+import userRoutes from './routes/userRoutes.js';
+import mealLogRoutes from './routes/mealLogRoutes.js';
+import insightRoutes from './routes/insightRoutes.js';
+
+dotenv.config();
+
 const app = express();
+app.use(express.json());
+app.set('json spaces', 2);
+
 const PORT = process.env.PORT || 5001;
 
-connectDB();
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/meallogs', mealLogRoutes);
+app.use('/api/insights', insightRoutes);
+
+// Connect to mongoDB
+(async () => {
+  try {
+    await connectDB();
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+})();
 
 app.get('/', (req, res) => {
   res.send('DailyNutri Backend is running!');
