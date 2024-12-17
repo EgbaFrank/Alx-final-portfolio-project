c673077e362618ab196d8a58d424f4f6b8e76de0
Egba Frank <Egbafrank72@gmail.com>
2024-12-17 02:56:50 +0000

    chore: use env variables for database URI

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect('mongodb://localhost:27017/daily-nutri');
+    const connection = await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected successfully:', connection.connection.host);
   } catch (err) {
     console.error('Error connecting to mongoDB:', err.message);
