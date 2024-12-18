dc4c184ba6b0a48d5dfe088d161e1cfe904cb655
Egba Frank <Egbafrank72@gmail.com>
2024-12-18 12:20:10 +0000

    refactor: change recommendation to more concise tip

--- backend/models/Tips.js
+++ backend/models/Tips.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 
-const recommendationSchema = new mongoose.Schema({
+const tipSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true },
   alertId: { type: mongoose.Schema.Types.ObjectId, ref: 'Alerts', required: true },
   status: { type: String, enum: ['active', 'fulfilled'], default: 'active' },
   message: { type: String, required: true },
 }, { timestamps: true });
 
-recommendationSchema.set('toJSON', {
+tipSchema.set('toJSON', {
   transform: (doc, ret) => {
     const newRet = { ...ret };
     delete newRet.__v;
@@ -15,6 +15,6 @@ recommendationSchema.set('toJSON', {
   },
 });
 
-recommendationSchema.index({ userId: 1, status: 1 });
+tipSchema.index({ userId: 1, status: 1 });
 
-export default mongoose.model('Recommendations', recommendationSchema);
+export default mongoose.model('Tips', tipSchema);
