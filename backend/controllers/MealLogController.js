73108c7ca702a5b15c36a74e00d6ffcc2fea7c90
Egba Frank <Egbafrank72@gmail.com>
2024-12-20 23:10:47 +0000

    feat: add meal log processor

--- backend/controllers/MealLogController.js
+++ backend/controllers/MealLogController.js
@@ -1,4 +1,6 @@
 import MealLog from '../models/MealLog.js';
+import { fetchNutrientData } from '../services/nutritionAPI.js';
+import Insight from './InsightController.js';
 
 class MealLogController {
   static async createMealLog(req, res) {
@@ -49,6 +51,42 @@ class MealLogController {
     } catch (err) {
       console.error(err);
       return res.status(500).json({ error: err.message });
+    }
+  }
+
+  static async processMealLog(userId, mealData) {
+    try {
+      if (!mealData || !mealData.id || !mealData.portionSize) {
+        throw new Error('Invalid meal data provided.');
+      }
+
+      // retrieve meal nutrient info
+      const { microNutrientData, macroNutrientData } = await fetchNutrientData(mealData);
+
+      if (!microNutrientData || !macroNutrientData) {
+        throw new Error('Nutrient Data could not be retrieved for meal.');
+      }
+
+      // Get daily and weekly active insight
+      const [dailyInsight, weeklyInsight] = await Promise.all([
+        Insight.getActiveInsight(userId, 'daily'),
+        Insight.getActiveInsight(userId, 'weekly'),
+      ]);
+
+      if (!dailyInsight || !weeklyInsight) {
+        throw new Error('Could not fetch or create an active insight.');
+      }
+
+      // update daily and weekly active insight with meal nutrient data
+      await Promise.all([
+        Insight.updateInsight(dailyInsight, macroNutrientData),
+        Insight.updateInsight(weeklyInsight, microNutrientData),
+      ]);
+
+      return { dailyInsight, weeklyInsight };
+    } catch (err) {
+      console.error(`Error processing meal log: ${err.message}`);
+      throw err;
     }
   }
 }
