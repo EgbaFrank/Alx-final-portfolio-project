27dfdd960fd615ed961d9a4e96da0ef91936eb13
Egba Frank <Egbafrank72@gmail.com>
2024-12-23 11:50:15 +0000

    feat: refactor mealLog schema to use recipes for logs

--- backend/models/MealLog.js
+++ backend/models/MealLog.js
@@ -4,28 +4,7 @@ const mealLogSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true },
   mealName: { type: String, required: true },
   mealType: { type: String, enum: ['Breakfast', 'Lunch', 'Dinner', 'Snacks'], required: true },
-  ingredients: [
-    {
-      name: { type: String, required: true },
-      portionSize: {
-        value: {
-          type: Number, min: 1, default: 100, required: true,
-        },
-        unit: {
-          type: String, enum: ['gram', 'cup', 'bowl'], default: 'gram', required: true,
-        },
-      },
-      nutrients: {
-        type: Map,
-        of: new mongoose.Schema({
-          nutrientName: { type: String, required: true },
-          unitName: { type: String, required: true },
-          value: { type: Number, required: true },
-        }),
-      },
-    },
-  ],
-  notes: { type: String },
+  recipe: { type: mongoose.Schema.Types.ObjectId, ref: 'Recipes', required: true },
 }, { timestamps: true });
 
 mealLogSchema.set('toJSON', {
