c2e7e75127b26e22265f4a57d654a9845cd5861d
Egba Frank <Egbafrank72@gmail.com>
2024-12-23 13:25:45 +0000

    feat: add nutrients list to look out for

---
+++ backend/utils/nutrients.js
@@ -0,0 +1,53 @@
+const nutrients = {
+  Protein: {
+    usdaId: 1003,
+    recommended: {
+      amount: 50,
+      unit: 'G',
+    },
+  },
+  Carbohydrate: {
+    usdaId: 1005,
+    recommended: {
+      amount: 275,
+      unit: 'G',
+    },
+  },
+  Fat: {
+    usdaId: 1004,
+    recommended: {
+      amount: 70,
+      unit: 'G',
+    },
+  },
+  'Vitamin C': {
+    usdaId: 1162,
+    recommended: {
+      amount: 90,
+      unit: 'MG',
+    },
+  },
+  Calcium: {
+    usdaId: 1087,
+    recommended: {
+      amount: 1000,
+      unit: 'MG',
+    },
+  },
+  Iron: {
+    usdaId: 1089,
+    recommended: {
+      amount: 18,
+      unit: 'MG',
+    },
+  },
+  'Vitamin D': {
+    usdaId: 1114,
+    recommended: {
+      amount: 20,
+      unit: 'MG',
+    },
+  },
+};
+
+export default nutrients;
