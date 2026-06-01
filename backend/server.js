fbc56ee57de96e5323d98146ae84f69bfd55a401
Egba Frank <Egbafrank72@gmail.com>
2026-06-01 01:42:33 +0100

    refactor: update packages

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,37 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
-import connectDB from './utils/db.js';
+import dotenv from "dotenv";
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import connectDB from "./utils/db.js";
+import morgan from "morgan";
 
-import userRoutes from './routes/userRoutes.js';
-import mealLogRoutes from './routes/mealLogRoutes.js';
-import insightRoutes from './routes/insightRoutes.js';
-import alertRoutes from './routes/alertRoutes.js';
-import tipRoutes from './routes/tipRoutes.js';
-import recipeRoutes from './routes/recipeRoutes.js';
+import userRoutes from "./routes/userRoutes.js";
+import mealLogRoutes from "./routes/mealLogRoutes.js";
+import insightRoutes from "./routes/insightRoutes.js";
+import alertRoutes from "./routes/alertRoutes.js";
+import tipRoutes from "./routes/tipRoutes.js";
+import recipeRoutes from "./routes/recipeRoutes.js";
 
 dotenv.config();
 
-const swaggerDoc = YAML.load('./docs/api.yaml');
+const swaggerDoc = YAML.load("./docs/api.yaml");
 
 const app = express();
 app.use(express.json());
-app.set('json spaces', 2);
+app.set("json spaces", 2);
+
+app.use(morgan("[:date[iso]] :method :url :status :response-time ms"));
 
 const PORT = process.env.PORT || 5001;
 
 // Routes
-app.use('/api/users', userRoutes);
-app.use('/api/meallogs', mealLogRoutes);
-app.use('/api/insights', insightRoutes);
-app.use('/api/alerts', alertRoutes);
-app.use('/api/tips', tipRoutes);
-app.use('/api/recipes', recipeRoutes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use("/api/users", userRoutes);
+app.use("/api/meallogs", mealLogRoutes);
+app.use("/api/insights", insightRoutes);
+app.use("/api/alerts", alertRoutes);
+app.use("/api/tips", tipRoutes);
+app.use("/api/recipes", recipeRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 // Connect to mongoDB
 (async () => {
@@ -40,11 +43,13 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
   }
 })();
 
-app.get('/', (req, res) => {
-  res.send('DailyNutri Backend is running!');
+app.get("/", (req, res) => {
+  res.send("DailyNutri Backend is running!");
 });
 
 app.listen(PORT, () => {
+  console.log("=================================");
   console.log(`Server running on port ${PORT}`);
   console.log(`API docs avaliable at http://localhost:${PORT}/api-docs`);
+  console.log("=================================");
 });
