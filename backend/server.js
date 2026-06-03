275e9e28cc6eb633cbae116bf6b75a1a2d53561c
Egba Frank <Egbafrank72@gmail.com>
2026-06-03 13:33:56 +0100

    refactor(app): move server logic to app.js for better structure and maintainability

--- backend/server.js
+++ backend/server.js
@@ -1,54 +1,6 @@
-import dotenv from "dotenv";
-import express from "express";
-import swaggerUi from "swagger-ui-express";
-import YAML from "yamljs";
-import connectDB from "./utils/db.js";
-import morgan from "morgan";
-
-import userRoutes from "./routes/userRoutes.js";
-import mealLogRoutes from "./routes/mealLogRoutes.js";
-import insightRoutes from "./routes/insightRoutes.js";
-import alertRoutes from "./routes/alertRoutes.js";
-import tipRoutes from "./routes/tipRoutes.js";
-import recipeRoutes from "./routes/recipeRoutes.js";
-import dns from "dns";
-
-dotenv.config();
-
-const swaggerDoc = YAML.load("./docs/api.yaml");
-
-dns.setDefaultResultOrder("ipv4first");
-
-const app = express();
-app.use(express.json());
-app.set("json spaces", 2);
-
-app.use(morgan("[:date[iso]] :method :url :status :response-time ms"));
+import app from "./app.js";
 
 const PORT = process.env.PORT || 5001;
-
-// Routes
-app.use("/api/users", userRoutes);
-app.use("/api/meallogs", mealLogRoutes);
-app.use("/api/insights", insightRoutes);
-app.use("/api/alerts", alertRoutes);
-app.use("/api/tips", tipRoutes);
-app.use("/api/recipes", recipeRoutes);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
-
-// Connect to mongoDB
-(async () => {
-  try {
-    await connectDB();
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-})();
-
-app.get("/", (req, res) => {
-  res.send("DailyNutri Backend is running!");
-});
 
 app.listen(PORT, () => {
   console.log("=================================");
